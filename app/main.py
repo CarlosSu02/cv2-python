dbda4137274e99e3d1cd5c7f00b9dd72542e15c2
CarlosSu02 <carlossuplei@gmail.com>
2024-07-11 15:08:49 -0600

    chore: FastAPI and SocketIO with opencv not working :((((((

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 
+import base64
+from threading import Thread
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from jinja2 import Template
+import numpy as np
 import uvicorn
 import socketio
 from fastapi.templating import Jinja2Templates
+import cv2
 
 app = FastAPI();
 
@@ -14,6 +17,8 @@ socket_app = socketio.ASGIApp(sio)
 app.mount('/socket.io', socket_app)
 
 templates = Jinja2Templates(directory='app/templates')
+
+current_frame = None
 
 @app.get('/', response_class=HTMLResponse)
 async def root(req:Request):
@@ -28,5 +33,27 @@ async def connect(sid, env):
 async def disconnect(sid):
     print(f'Client disconnected, id: {str(sid)}') 
 
+@sio.on('frame')
+async def handle_frame(sid, data):
+    global current_frame
+    img_data = base64.b64decode(data.split(',')[1])
+    array = np.frombuffer(img_data, np.uint8) # Convert the image data to a NumPy array
+    frame = cv2.imdecode(array, cv2.IMREAD_COLOR) # Decode the image
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    current_frame = gray
+    print(f"Received data: {data[:100]}...") 
+
+def display_frames():
+    global current_frame
+    # print type current_frame
+    while True:
+        if current_frame is not None:
+            cv2.imshow('Frame', current_frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+
 if __name__ == '__main__':
+    display_thread = Thread(target=display_frames, daemon=True) # Create a thread for displaying frames
+    display_thread.start() # Start the thread
     uvicorn.run('main:app', host='0.0.0.0', port=8080, reload=True, workers=2)
