d5fc39ea50f2ba334e511d8b051f35e421de982c
Carlos Su <carlossuplei@gmail.com>
2024-08-19 16:11:02 -0600

    feat: arduino cv2-ino-2.

--- arduino-codes/cv2-ino-2/cv2-ino-2.ino
+++ arduino-codes/cv2-ino-2/cv2-ino-2.ino
@@ -14,7 +14,7 @@ long duration;
 int distance;
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600);
 
   pinMode(Led,OUTPUT); 
@@ -33,45 +33,11 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  // py();
-  // digitalWrite(pinLed, HIGH);
 
   serRead();
   fotocelda();
 
 }
-/*
-void py () {
-
-
-  if (Serial.available() > 0) {
-
-    char option = Serial.read();
-
-    if (option >= '1' && option <= '9') {
-   
-      option -= '0';
-    
-      // for (int i = 0; i < option; i++) {
-   
-        digitalWrite(pinLed, HIGH);
-        delay(100);
-   
-        // digitalWrite(pinLed, LOW);
-        // delay(100);
-   
-      // }
-   
-    }
-
-  }
-
-  digitalWrite(pinLed, LOW);
-  // delay(100);
-
-}*/
 
 void serRead () {
 
@@ -118,67 +84,26 @@ void serRead () {
     
   }
 
-  // if (Serial.available() <= 0)
-  //   return;
-
-  // char count = Serial.read();
-
-  // if (count < '1' || count > '5')
-  //   return onLeds(0, false);
-
-  // count -= '0';
-  
-  // return onLeds(count, true);
-  
-  // Serial.println(int(count));
-  
-  /*
-  for (int i = 0; i < count; i++) {
-  
-    Serial.println(i);
-    
-  }
-  */
-  
 }
 
 void onLeds (int count, bool on) {
-
-  // Serial.println('5' - count);
-  // Serial.println(count + '1');
-    
-  // Serial.print("count: ");
-  // Serial.println(int(count));
-  // Serial.print("on: ");
-  // Serial.println(String(on));
   
   if (count < 0)
     return;
-  
-  // Serial.println(count);
-
-  //count -= '0';
-  
-  //Serial.println(count);
-  
+    
   if (count < 5)
     for (int i = (count + 5); i <= pinLast; i++) {
 
       digitalWrite(i, count == 0 ? LOW : on ? LOW : HIGH);
-      // delay(100);
-
+  
     }
 
   for (int i = pinInit; i <= (count + 4); i++) {
   
-    // Serial.println(i);
     digitalWrite(i, on ? HIGH : LOW);
-    // delay(50);
     
   }
-  
-  // onLeds(serRead(), false);
-  
+    
 }
 
 void fotocelda () {
